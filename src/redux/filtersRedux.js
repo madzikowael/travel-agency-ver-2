30d0e70304a50a9a49091667b4f068a6bd76bd4d
Magda Lejwoda <le.magda@o2.pl>
2019-12-18 21:51:38 +0100

    Add filtres of travels

--- src/redux/filtersRedux.js
+++ src/redux/filtersRedux.js
@@ -11,10 +11,20 @@ const createActionName = name => `app/${reducerName}/${name}`;
 // action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
 // TODO - add other action types
+export const ADD_TAGS = createActionName('ADD_TAGS');
+export const REMOVE_TAGS = createActionName('REMOVE_TAGS');
+export const CHANGE_DURATION = createActionName('CHANGE_DURATION');
 
 // action creators
 export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
 // TODO - add other action creators
+export const addTags = payload => ({ payload, type: ADD_TAGS });
+export const removeTags = payload => ({ payload, type: REMOVE_TAGS });
+export const changeDuration = (frame, value) => ({
+  frame,
+  value,
+  type: CHANGE_DURATION,
+});
 
 // reducer
 export default function reducer(statePart = [], action = {}) {
@@ -25,6 +35,24 @@ export default function reducer(statePart = [], action = {}) {
         searchPhrase: action.payload,
       };
     // TODO - handle other action types
+    case ADD_TAGS:
+      return {
+        ...statePart,
+        tags: [...statePart.tags, action.payload],
+      };
+    case REMOVE_TAGS:
+      return {
+        ...statePart,
+        tags: [...statePart.tags.filter(element => element !== action.payload)],
+      };
+    case CHANGE_DURATION:
+      return {
+        ...statePart,
+        duration: {
+          ...statePart.duration,
+          [action.frame]: parseInt(action.value),
+        },
+      };
     default:
       return statePart;
   }
